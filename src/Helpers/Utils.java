0dac195f40acb06a96e7f06946040bb173143dd8
rogueeeee <61908580+rogueeeee@users.noreply.github.com>
2021-01-09 11:35:39 +0800

    added Helpers package and Utils class, added Utils.GenerateRandomString()

---
+++ src/Helpers/Utils.java
@@ -0,0 +1,23 @@
+package Helpers;
+
+import java.util.Random;
+
+public class Utils
+{
+    static final Random RngInstance = new Random();
+    
+    public static String GenerateRandomString(int Length)
+    {
+        return GenerateRandomString(Length, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+    }
+    
+    public static String GenerateRandomString(int Length, String CharSet)
+    {
+        String Result = "";
+        
+        for (int i = 0; i < Length; i++)
+            Result += CharSet.charAt(RngInstance.nextInt(CharSet.length() - 1));
+        
+        return Result;
+    }
+}
